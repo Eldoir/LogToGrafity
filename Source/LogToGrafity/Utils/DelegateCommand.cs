1bba8cfc53c44fe0ffdaeeb283bd3f098f413c8a
Arthur Cousseau <arthur.cousseau@me.com>
2023-10-07 15:42:07 +0200

    Add Utils: DelegateCommand and Result

---
+++ Source/LogToGrafity/Utils/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LogToGrafity
+{
+    public class DelegateCommand : ICommand
+    {
+        public DelegateCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #region ICommand
+        
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute?.Invoke();
+        }
+
+        #endregion
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+    }
+}
---
+++ Source/LogToGrafity/Utils/Result.cs
@@ -0,0 +1,62 @@
+﻿namespace LogToGrafity
+{
+    public interface IResult
+    {
+        bool IsSuccess { get; }
+        bool IsFailure { get; }
+
+        /// <summary>
+        /// Only set when <see cref="IsFailure"/>.
+        /// </summary>
+        string Message { get; }
+    }
+
+    public struct Result : IResult
+    {
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure => !IsSuccess;
+        public string Message { get; private set; }
+
+        public static Result Ok()
+        {
+            return new Result { IsSuccess = true };
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return Result<T>.Ok(value);
+        }
+
+        public static Result Fail(string message)
+        {
+            return new Result { IsSuccess = false, Message = message };
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return Result<T>.Fail(message);
+        }
+    }
+
+    public struct Result<T> : IResult
+    {
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure => !IsSuccess;
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Only set when <see cref="IsSuccess"/>.
+        /// </summary>
+        public T Value { get; private set; }
+
+        public static Result<T> Ok(T value)
+        {
+            return new Result<T> { IsSuccess = true, Value = value };
+        }
+
+        public static Result<T> Fail(string message)
+        {
+            return new Result<T> { IsSuccess = false, Message = message };
+        }
+    }
+}
