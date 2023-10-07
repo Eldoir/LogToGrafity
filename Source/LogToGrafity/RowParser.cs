ecf04c1994ff492c1273bc6ef306b0db0785e05e
Arthur Cousseau <arthur.cousseau@me.com>
2023-10-07 15:45:44 +0200

    RowParser + plumbing with dialogs and MessageBox

---
+++ Source/LogToGrafity/RowParser.cs
@@ -0,0 +1,28 @@
+﻿namespace LogToGrafity
+{
+    public record Row(string Freq, double Temp, string Eps1, string Eps2);
+
+    public interface IRowParser
+    {
+        Result<Row> Parse(string row);
+    }
+
+    public class RowParser : IRowParser
+    {
+        public Result<Row> Parse(string row)
+        {
+            string[] values = row.Trim().Split('\t');
+            if (values.Length < 8)
+                return Result.Fail<Row>("Row needs at least 8 values since, eps1 and eps2 are the 7th and 8th values");
+
+            string tempStr = values[1];
+            if (!double.TryParse(tempStr, out double temp))
+                return Result.Fail<Row>($"Unknown temperature: {tempStr}");
+
+            string freq = values[0];
+            string eps1 = values[6];
+            string eps2 = values[7];
+            return Result.Ok(new Row(freq, temp, eps1, eps2));
+        }
+    }
+}
--- Source/LogToGrafity/ViewModels/MainViewModel.cs
+++ Source/LogToGrafity/ViewModels/MainViewModel.cs
@@ -1,7 +1,115 @@
-﻿namespace LogToGrafity
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LogToGrafity
 {
     class MainViewModel
     {
-        public string Text { get; set; } = "couc";
+        public MainViewModel()
+        {
+            OpenFileCommand = new DelegateCommand(OpenFile);
+            _rowParser = new RowParser();
+            _rowAnalyzer = new RowAnalyzer();
+        }
+
+        public ICommand OpenFileCommand { get; }
+
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = "Log files (*.log)|*.log|All files (*.*)|*.*"
+            };
+            if (openFileDialog.ShowDialog() != true)
+                return;
+
+            Result<string> result = ParseFile(openFileDialog.FileName);
+            if (result.IsFailure)
+            {
+                ShowError(result.Message);
+                return;
+            }
+
+            ShowSuccess("Now select a folder in which files eps1.csv and eps2.csv will be saved.");
+
+            System.Windows.Forms.FolderBrowserDialog dialog = new()
+            {
+                RootFolder = System.Environment.SpecialFolder.Desktop,
+                ShowNewFolderButton = true
+            };
+            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            string eps1FileName = "eps1.csv";
+            string eps2FileName = "eps2.csv";
+            string directoryPath = dialog.SelectedPath;
+
+            while (DirectoryContains(directoryPath, eps1FileName) || DirectoryContains(directoryPath, eps1FileName))
+            {
+                if (MessageBox.Show(
+                    $"{eps1FileName} or {eps2FileName} already exist. Do you want to overwrite them?",
+                    "Overwrite?",
+                    MessageBoxButton.YesNo,
+                    MessageBoxImage.Question) == MessageBoxResult.No)
+                {
+                    if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                        return;
+
+                    directoryPath = dialog.SelectedPath;
+                }
+                else break;
+            }
+
+            File.WriteAllText(Path.Join(directoryPath, eps1FileName), result.Value);
+            File.WriteAllText(Path.Join(directoryPath, eps2FileName), result.Value); // TODO: get result
+            ShowSuccess("Saved!");
+        }
+
+        private static bool DirectoryContains(string directoryPath, string fileName)
+        {
+            return File.Exists(Path.Join(directoryPath, fileName));
+        }
+
+        private static void ShowSuccess(string message)
+        {
+            MessageBox.Show(message, "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private static void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private Result<string> ParseFile(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length < 4)
+                return Result.Fail<string>(
+                    "File must be at least 4 lines (2 header lines, 1 line for column names, and at least 1 line of data)");
+
+            List<Row> rows = new();
+
+            for (int i = 3; i < lines.Length; i++)
+            {
+                if (string.IsNullOrEmpty(lines[i]))
+                    continue;
+
+                Result<Row> rowResult = _rowParser.Parse(lines[i]);
+                if (rowResult.IsFailure)
+                    return Result.Fail<string>(rowResult.Message);
+
+                rows.Add(rowResult.Value);
+            }
+
+            FileResult result = _rowAnalyzer.Analyze(rows);
+
+            return Result.Ok("couc");
+        }
+
+        private readonly IRowParser _rowParser;
+        private readonly IRowAnalyzer _rowAnalyzer;
     }
 }
