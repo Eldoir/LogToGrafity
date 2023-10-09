71ed27b1091702795aaa35b3449f6e5cbab3e6c0
Arthur Cousseau <arthur.cousseau@me.com>
2023-10-09 18:09:18 +0200

    Add DelegateCommand<T>

--- Source/LogToGrafity/Utils/DelegateCommand.cs
+++ Source/LogToGrafity/Utils/DelegateCommand.cs
@@ -35,4 +35,40 @@ namespace LogToGrafity
         private readonly Action _execute;
         private readonly Func<bool>? _canExecute;
     }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public DelegateCommand(Action<T> execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #region ICommand
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter is T value)
+            {
+                _execute?.Invoke(value);
+            }
+        }
+
+        #endregion
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private readonly Action<T> _execute;
+        private readonly Func<bool>? _canExecute;
+    }
 }
